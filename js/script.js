a7646cb4121adc339b89f353b1da7b897e6a469a
yoelandres <yoel.a.p.2007@gmail.com>
2026-08-18 16:03:48 +0200

    Update final message for countdown

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const fechaObjetivo = new Date("2026-08-13T00:00:00");
-const fraseFinal = "¡HASTA EL AÑO QUE VIENE!";
-const fraseFinal = "¡GRACAIS A TODOS!";
+const fraseFinal = "¡GRACIAS A TODOS, HASTA EL AÑO QUE VIENE!";
 
 function actualizarContador() {
   const ahora = new Date();
